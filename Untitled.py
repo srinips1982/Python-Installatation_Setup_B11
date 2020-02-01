25e2b7051ad06bf33fed04b8a9c9b030fd031fab
srinips1982 <60518615+srinips1982@users.noreply.github.com>
2020-02-01 16:45:19 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print'Hello, your initiative of teaching Python over online is much appreciatiated'
+
+
+# In[2]:
+
+
+print ('Hello, your initiative of teaching Python over online is much appreciatiated')
+
+
+# In[ ]:
+
+
+
+
